24e5a4116949e6c46664b69045304b0c5830f7ca
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-16 23:56:03 -0400

    run `update`

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * is-primitive <https://github.com/jonschlinkert/is-primitive>
  *
- * Copyright (c) 2014-2015 Jon Schlinkert.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
